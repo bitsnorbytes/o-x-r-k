2d6c32385034d1ed9a6d2233b94e69cda03d9eaf
Punyasloka Parida <12557052+bitsnorbytes@users.noreply.github.com>
2023-06-23 19:14:12 +0530

    convoluted login flow

--- granary/Pages/Index.cshtml.cs
+++ granary/Pages/Index.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Supabase;
 using Granary.Models;
-// using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace Granary.Pages;
 
@@ -14,6 +14,9 @@ public class IndexModel : PageModel
     public string WarningMessages { get; set;}
     public const string SessionKeyId = "_Id";
     public const string SessionKeyAge = "_Age";
+    public const string SessionAccessToken = "_AccessToken";
+    public const string UserEmailConfirmationSent = "_UserEmailConfirmationSent";
+    public const string UserEmailConfirmationReceived = "_UserEmailConfirmationReceived";
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger, Client client)
@@ -24,17 +27,39 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+      if(HttpContext.Session.GetString(SessionKeyId) != string.Empty) {
+        if (HttpContext.Session.GetString(UserEmailConfirmationReceived) == string.Empty) {
+          WarningMessages = "You need to confirm your email!";
+        }
+      }
     }
     [BindProperty]
     public User? User { get; set; }
-
+    string StringNullChecker(string? input) {
+        var output = string.Empty;
+        if (input != null) {
+        output = input.ToString();
+        }
+        return output;
+    }
+    string DateNullChecker(DateTime? input) {
+        var output = string.Empty;
+        if (input != null) {
+        output = input.ToString();
+        }
+        return output;
+    }
     public async Task<IActionResult> OnPostAsync()
     {
        HttpContext.Session.Clear();
        try{
          var session = await _client.Auth.SignIn(User.Email, User.Password);
-         HttpContext.Session.SetString(SessionKeyId, session.User.Id);
+         HttpContext.Session.SetString(SessionKeyId, StringNullChecker(session.User.Id));
+         HttpContext.Session.SetString(SessionAccessToken, StringNullChecker(session.AccessToken));
+         HttpContext.Session.SetString(UserEmailConfirmationSent, DateNullChecker(session.User.ConfirmationSentAt));
+         HttpContext.Session.SetString(UserEmailConfirmationReceived, DateNullChecker(session.User.ConfirmedAt));
          HttpContext.Session.SetInt32(SessionKeyAge, session.ExpiresIn);
+         
        } catch (Exception err){
         
         if(err.Message.Contains("Email not confirmed")) {
--- granary/Pages/Signup.cshtml.cs
+++ granary/Pages/Signup.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Supabase;
 using Granary.Models;
-// using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace Granary.Pages;
 
@@ -12,6 +12,9 @@ public class SignupModel : PageModel
     private readonly Client _client;
     public const string SessionKeyId = "_Id";
     public const string SessionKeyAge = "_Age";
+    public const string SessionAccessToken = "_AccessToken";
+    public const string UserEmailConfirmationSent = "_UserEmailConfirmationSent";
+    public const string UserEmailConfirmationReceived = "_UserEmailConfirmationReceived";
     private readonly ILogger<SignupModel> _logger;
 
     public SignupModel(ILogger<SignupModel> logger, Client client)
@@ -25,12 +28,29 @@ public class SignupModel : PageModel
     }
     [BindProperty]
     public User? User { get; set; }
-
+    
+    string StringNullChecker(string? input) {
+        var output = string.Empty;
+        if (input != null) {
+        output = input.ToString();
+        }
+        return output;
+    }
+    string DateNullChecker(DateTime? input) {
+        var output = string.Empty;
+        if (input != null) {
+        output = input.ToString();
+        }
+        return output;
+    }
     public async Task<IActionResult> OnPostAsync()
     {
        HttpContext.Session.Clear();
        var session = await _client.Auth.SignUp(User.Email, User.Password);
-       HttpContext.Session.SetString(SessionKeyId, session.User.Id);
+       HttpContext.Session.SetString(SessionKeyId, StringNullChecker(session.User.Id));
+       HttpContext.Session.SetString(SessionAccessToken, StringNullChecker(session.AccessToken));
+       HttpContext.Session.SetString(UserEmailConfirmationSent, DateNullChecker(session.User.ConfirmationSentAt));
+       HttpContext.Session.SetString(UserEmailConfirmationReceived, DateNullChecker(session.User.ConfirmedAt));
        HttpContext.Session.SetInt32(SessionKeyAge, session.ExpiresIn);
        return RedirectToPage("./Index");
     }
