a5c5f2e06a8a4bf01312b2939e85ea86211fb870
Punyasloka Parida <punyasloka.parida@seba.swiss>
2024-04-21 22:58:04 +0530

    Changes to error page

--- veil/src/routes/+page.svelte
+++ veil/src/routes/+page.svelte
@@ -6,7 +6,7 @@
   };
 </script>
 
-<div class="bg-slate-900 px-4 text-zinc-50">
+<div class="bg-slate-900 px-4 text-zinc-50 overflow-auto">
   <ul class="grid list-none grid-cols-4 gap-4 font-display">
     {#each data.movies as movie}
       <li class="max-w-80">
