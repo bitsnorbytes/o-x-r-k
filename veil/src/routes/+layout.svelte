747e6042caa4118c7d72985fce73f2c4b1d909bf
Punyasloka Parida <punyasloka.parida@seba.swiss>
2024-04-21 15:31:51 +0530

    Design changes

---
+++ veil/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Noto+Serif:ital,wght@0,427;1,427&display=swap" rel="stylesheet">
+</svelte:head>
+<script>
+  import "../style/app.css";
+</script>
+<slot />
---
+++ veil/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+
+<div class="bg-slate-950 w-auto h-screen text-zinc-50">
+    <div class="flex flex-row justify-between">
+        <h1 class="font-display text-4xl font-medium tracking-widest mx-8">oxrk.</h1>
+        <ul class="font-display-2 text-xl list-none flex flex-row justify-end items-center mx-8">
+            <li class="mx-4">movies near you</li>
+            <li class="mx-4">film festivals</li>
+            <li class="mx-4">search</li>
+            <li class="mx-4">login / sign-up</li>
+          </ul>
+    </div>
+    
+</div>
