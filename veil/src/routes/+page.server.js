78e5287d4b8b3c05e18931c5d7b49ca4904dd7a5
Punyasloka Parida <punyasloka.parida@seba.swiss>
2024-04-22 13:09:00 +0530

    Changes to az pipelines

--- veil/src/routes/+page.server.js
+++ veil/src/routes/+page.server.js
@@ -1,6 +1,12 @@
-import { supabase } from "$lib/supabaseClient";
+//import { supabase } from "$lib/supabaseClient";
+import { createClient } from "@supabase/supabase-js";
+import { env } from "$env/dynamic/private";
+const supabaseUrl = env.SUPABASE_URL;
+const supabaseKey = env.SUPABASE_ANON_KEY;
+
 
 export async function load() {
+  const supabase = createClient(supabaseUrl, supabaseKey); 
   const { data } = await supabase.from("Movies").select();
   return {
     movies: data ?? [],
