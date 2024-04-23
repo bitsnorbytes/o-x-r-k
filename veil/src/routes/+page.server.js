162d73556a9619076a1f517aa0ab93f823869c60
Punyasloka Parida <punyasloka.parida@seba.swiss>
2024-04-23 11:00:58 +0530

    Changes to supabase

--- veil/src/routes/+page.server.js
+++ veil/src/routes/+page.server.js
@@ -1,12 +1,6 @@
-//import { supabase } from "$lib/supabaseClient";
-import { createClient } from "@supabase/supabase-js";
-import { env } from "$env/dynamic/private";
-const supabaseUrl = env.SUPABASE_URL;
-const supabaseKey = env.SUPABASE_ANON_KEY;
-
+import { supabase } from "$lib/server/supabaseClient";
 
 export async function load() {
-  const supabase = createClient(supabaseUrl, supabaseKey); 
   const { data } = await supabase.from("Movies").select();
   return {
     movies: data ?? [],
