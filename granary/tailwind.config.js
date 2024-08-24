45f7b7ee8818fd16a6327ee0bde679e042099e7b
Punyaslok <12557052+bitsnorbytes@users.noreply.github.com>
2024-08-24 18:45:01 +0530

    blut theme introduced. 1.0

--- granary/tailwind.config.js
+++ granary/tailwind.config.js
@@ -1,17 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./Pages/**/*.{razor,html,cshtml}"
-],
-  theme: {
-    extend: {
-      fontFamily: {
-        'gy-body': ["Open Sans", "sans-serif"]
-      },
+    content: [
+        "./Pages/**/*.{razor,html,cshtml}"
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'gy-body': ["Open Sans", "sans-serif"]
+            },
+            colors: {
+                blute: {
+                    100: '#348888',
+                    200: '#22BABB',
+                    300: '#9EF8EE',
+                    400: '#FA7F08',
+                    500: '#F24405'
+}
+          },
     },
   },
-  plugins: [
+plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-  ],
+],
 }
